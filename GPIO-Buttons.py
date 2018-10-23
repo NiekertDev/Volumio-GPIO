8372830e2a0dac4b74114c8f944b12d711f4898b
Niekert <me@niekert.tk>
2018-10-23 21:21:03 +0200

    Update GPIO-Buttons.py

--- GPIO-Buttons.py
+++ GPIO-Buttons.py
@@ -4,7 +4,7 @@ from signal import pause
 
 # Edit these variables to match the corresponding pin
 
-mainpin = 2
+mainpin = 17
 previouspin = 23
 nextpin = 22
 voldownpin = 24
@@ -18,38 +18,44 @@ volup = Button(voluppin, hold_time=2)
 
 
 def toggle():
-   print("toggled")
+   print("Toggle button pressed")
    check_call(["volumio", "toggle", "&"])
-
+   print("\n")
 
 def previous():
-   print("previous")
+   print("Previous button pressed")
    check_call(["volumio", "previous"])
-
+   print("\n")
 
 def next():
-   print("next")
+   print("Next button pressed")
    check_call(["volumio", "next"])
+   print("\n")
 
 def volumedown():
-   print("volumedown")
+   print("Volume down button pressed")
    check_call(["volumio", "volume", "minus"])
+   print("\n")
 
 def volumeup():
-   print("volumeup")
+   print("Volume up button pressed")
    check_call(["volumio", "volume", "plus"])
+   print("\n")
 
 def volumefull():
-   print("volumefull")
+   print("Volume full button pressed")
    check_call(["volumio", "volume", "100"])
+   print("\n")
 
 def volumemute():
-   print("mute")
+   print("Mute button pressed")
    check_call(["volumio", "volume", "mute"])
+   print("\n")
 
 def halt():
-   print("halt")
+   print("Halt button pressed")
    check_call(["sudo", "halt"])
+   print("\n")
 
 main.when_pressed = toggle
 main.when_held = halt
